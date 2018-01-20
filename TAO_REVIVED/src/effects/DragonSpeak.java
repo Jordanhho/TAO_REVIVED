144c2d11c1bcdd66d02195858d65e36e96731b7e
jhho <Jordanhho12@hotmail.com>
2018-01-20 01:20:45 -0800

    fix duplicate dragonSpeak effect when more than 1 DSM created bug

--- TAO_REVIVED/src/effects/DragonSpeak.java
+++ TAO_REVIVED/src/effects/DragonSpeak.java
@@ -5,6 +5,7 @@
 package effects;
 
 import java.util.Iterator;
+
 import units.Pyromancer;
 import units.DragonSpeakerMage;
 import units.DragonTyrant;
@@ -14,27 +15,25 @@ import units.BasicUnit;
 public class DragonSpeak extends Effect
 {
     private BasicUnit unit;
-    
+
     public DragonSpeak(final BasicUnit unit) {
         super(unit);
         this.unit = unit;
     }
-    
+
     public int powerChange() {
         final Iterator<Unit> itr = this.unit.getPlayer().getBoard().iterator();
-        boolean hasDragon = false;
-        boolean hasDSM = false;
         int numPyros = 0;
+        int numDragons = 0;
+        int numDSMs = 0;
         while (itr.hasNext()) {
             final Unit u = itr.next();
             if (u.getPlayer() == this.unit.getPlayer()) {
                 if (u instanceof DragonTyrant) {
-                    hasDragon = true;
-                }
-                else if (u instanceof DragonSpeakerMage) {
-                    hasDSM = true;
-                }
-                else {
+                    numDragons++;
+                } else if (u instanceof DragonSpeakerMage) {
+                    numDSMs++;
+                } else {
                     if (!(u instanceof Pyromancer)) {
                         continue;
                     }
@@ -42,23 +41,122 @@ public class DragonSpeak extends Effect
                 }
             }
         }
-        if (!hasDragon || !hasDSM) {
+
+
+
+//        int powerPerUser = (numDSMs + numPyros) * numDragons * 12;
+//        int numUsers = numDSMs + numPyros;
+//        int totalDragonPower = numDragons * 28;
+
+        if (numDragons == 0 || numDSMs == 0) {
             this.end();
             return 0;
         }
-        if (numPyros < 2) {
+
+        int numSharedUsers = numPyros + numDSMs;
+        int numPowerToShare = numSharedUsers * numDragons * 12;
+        int totalDragonPower = numDragons * 28;
+
+
+        if(numPyros > 0 && numDSMs == 1) {  //Only 1 DSM and 1 pyro
             final int transfer = 12;
             if (this.unit instanceof DragonTyrant) {
                 return -transfer * (numPyros + 1);
             }
             return transfer;
         }
+//        else if(numPyros > 1 && numDSMs == 1) { //Only 1 DSM and many pyros
+//
+//        }
         else {
-            final int transfer = (int)Math.round(28.0 / (numPyros + 1));
-            if (this.unit instanceof DragonTyrant) {
-                return -28;
+            if(numPowerToShare > totalDragonPower) { //distribute total dragon power to all pyro and dsm
+                int numPowerToDistribute = (int)Math.ceil((float)totalDragonPower / (float)numSharedUsers);
+                if (this.unit instanceof DragonTyrant) {
+                    return -28;
+                }
+                return numPowerToDistribute;
             }
-            return transfer;
+            else { //otherwise distribute numPowertoGrab
+                int numPowerToDistribute = (int)Math.ceil((float)numPowerToShare / (float)numSharedUsers);
+                if (this.unit instanceof DragonTyrant) {
+                    //if(12 * numSharedUsers > numPowerToShare) {
+                    return -12 * numSharedUsers;
+                    //}
+                    //else {
+                    //return -12 * numSharedUsers;
+                    //}
+                }
+                return numPowerToDistribute;
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+//        if (numPyros < 2 && numDSMs == 1) {  //works!
+//            final int transfer = 12;
+//            if (this.unit instanceof DragonTyrant) {
+//                return -transfer * (numPyros + 1);
+//            }
+//            return transfer;
+//        }
+//        else {  //more than one dragon speaker
+//
+//
+//
+//            final int transfer = (int) Math.ceil(28.0 / (numPyros + numDSMs));
+//            System.out.println("transfer is: " + transfer);
+//            if (this.unit instanceof DragonTyrant) {
+//                return -100;
+//            }
+//            return transfer;
+//        }
+//        else if(numPyros > 1 && numDSMs > 1) {
+//
+//        }
+//
+//        else {
+//            final int transfer = (int) Math.ceil(28.0 / (numPyros + 1));
+//            if (this.unit instanceof DragonTyrant) {
+//                return -28;
+//            }
+//            return transfer;
+//        }
     }
 }
+
+
+        /*
+        DSM = 2
+        Pyro = 0
+        Dragons = 4
+        powerShare = 2 * 12 * 4 =
+         */
+//        if (numPowerToShare < totalDragonPower) {
+//            final int transfer = 12;
+//            //final int transfer = (int)Math.ceil(28.0 / (numPyros + 1));
+//            if (this.unit instanceof DragonTyrant) {
+//                return -28;
+//            }
+//            return transfer;
+//            if (this.unit instanceof DragonTyrant) {
+//                return -transfer * (numPyros + 1);
+//            }
+//            return transfer;
+
+
+//        } else {
+//            final int transfer = (int)Math.ceil(28.0 / (numPyros + 1));
+//            if (this.unit instanceof DragonTyrant) {
+//                return -28;
+//            }
+//            return transfer;
+//        }
+//    }
--- TAO_REVIVED/src/game/Board.java
+++ TAO_REVIVED/src/game/Board.java
@@ -5,6 +5,8 @@
 package game;
 
 import java.util.Set;
+
+import units.DragonSpeakerMage;
 import util.Direction;
 import util.ImageManager;
 import units.BasicUnit;
@@ -18,7 +20,12 @@ public class Board
     private ArrayList<Unit> units;
     private Player player1;
     private Player player2;
-    
+    private boolean dragonSpeakInit = false;
+
+    public boolean isDragonSpeakInit() {
+        return dragonSpeakInit;
+    }
+
     public Iterator<Unit> iterator() {
         return this.units.iterator();
     }
@@ -46,9 +53,17 @@ public class Board
             this.units.add(unit);
         }
         for (final Unit u : this.units) {
-            if (u instanceof BasicUnit) {
+            if(!(u instanceof DragonSpeakerMage)) {
                 ((BasicUnit)u).init();
             }
+            if(!dragonSpeakInit && u instanceof DragonSpeakerMage) {
+                ((BasicUnit)u).init();
+                System.out.println("init dragonSpeak!");
+                dragonSpeakInit = true;
+            }
+//            if (u instanceof BasicUnit) {
+//                ((BasicUnit)u).init();
+//            }
         }
         for (final Unit u : this.units) {
             if (u instanceof BasicUnit) {
@@ -81,8 +96,15 @@ public class Board
         }
         for (final Unit u : this.units) {
             if (u instanceof BasicUnit) {
-                ((BasicUnit)u).init();
-            }
+                if(!(u instanceof DragonSpeakerMage)) {
+                    ((BasicUnit)u).init();
+                }
+//                if(!dragonSpeakInit && u instanceof DragonSpeakerMage) {
+//                    ((BasicUnit)u).init();
+//                    System.out.println("init dragonSpeak!");
+//                    dragonSpeakInit = true;
+//                }
+          }
         }
         for (final Unit u : this.units) {
             if (u instanceof BasicUnit) {
